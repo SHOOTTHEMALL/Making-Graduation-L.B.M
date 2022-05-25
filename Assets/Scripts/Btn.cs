3aaf1c317e8a9bf400329ec9639d5d9c6e1f26c1
김예리나 <ggm.s200001@ggh.goe.go.kr>
2022-05-25 11:56:47 +0900

    얼굴사진씬 작업중입니다

---
+++ Assets/Scripts/Btn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Btn : MonoBehaviour
+{
+    public List<Image> images = new List<Image>();
+
+    public void LeftBtn()
+    {
+
+    }
+
+    public void RightBtn()
+    {
+
+    }
+}
