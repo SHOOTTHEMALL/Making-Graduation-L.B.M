dcbf79e25391fe79436fc1a5590529f58b2add0d
김예리나 <ggm.s200001@ggh.goe.go.kr>
2022-06-08 10:30:52 +0900

    기본적인 엘범 세팅 완료, 미니게임 개발중

--- Assets/Scripts/Btn.cs
+++ Assets/Scripts/Btn.cs
@@ -5,15 +5,24 @@ using UnityEngine.UI;
 
 public class Btn : MonoBehaviour
 {
-    public List<Image> images = new List<Image>();
+    public List<GameObject> images = new List<GameObject>();
+    int current = 0;
 
     public void LeftBtn()
     {
+        if (current == 0) return;
 
+        images[current].gameObject.SetActive(false);
+        images[current -1].gameObject.SetActive(true);
+        current--;
     }
 
     public void RightBtn()
     {
+        if (current == 2) return;
 
+        images[current].gameObject.SetActive(false);
+        images[current + 1].gameObject.SetActive(true);
+        current++;
     }
 }
